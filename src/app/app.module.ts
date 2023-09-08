a532c48a74ae852639bc314255002965147bd947
Hellen Brunelli <hellenbrunelli01@gmail.com>
2023-09-08 16:45:14 -0300

    initial structures created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { HeroBannerComponent } from './components/hero-banner/hero-banner.component';
+import { AdsPlaceComponent } from './components/ads-place/ads-place.component';
+import { CardComponent } from './components/card/card.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    HeroBannerComponent,
+    AdsPlaceComponent,
+    CardComponent,
+    FooterComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/ads-place/ads-place.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ads-place',
+  templateUrl: './ads-place.component.html',
+  styleUrls: ['./ads-place.component.scss']
+})
+export class AdsPlaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
