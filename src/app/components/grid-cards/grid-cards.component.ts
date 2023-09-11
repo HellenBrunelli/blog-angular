21c5afccef899ce5ec81079853678f4bb7d74437
Hellen Brunelli <hellenbrunelli01@gmail.com>
2023-09-11 18:23:37 -0300

    [GRID CARDS] Responsive grid cards structure created.

---
+++ src/app/components/grid-cards/grid-cards.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-cards',
+  templateUrl: './grid-cards.component.html',
+  styleUrls: ['./grid-cards.component.scss']
+})
+export class GridCardsComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
