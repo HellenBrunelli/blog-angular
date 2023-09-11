6650897ca25a828645bdc404429fa7d47222b444
Hellen Brunelli <hellenbrunelli01@gmail.com>
2023-09-11 18:21:57 -0300

    [FOOTER] Responsive footer structure created.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, HostListener } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
+  isMobile:boolean = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.checkScreenSize();
+  }
+  
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.checkScreenSize();
   }
 
+  private checkScreenSize():void {
+    const screenWidth = window.innerWidth;
+    const mobileWidth = 768;
+
+    if (screenWidth < mobileWidth) {
+      this.isMobile = true;
+    } else {
+      this.isMobile = false;
+    }
+  }
 }
