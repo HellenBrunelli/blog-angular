f7304e6b5ad0da056098f41c708c150c5cf8a140
Hellen Brunelli <hellenbrunelli01@gmail.com>
2023-09-13 11:52:49 -0300

    [GRID CARDS]  Updated cards to dynamic cards with mock data.

--- src/app/components/grid-cards/grid-cards.component.ts
+++ src/app/components/grid-cards/grid-cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { dataMock } from 'src/app/data/dataMock';
 
 @Component({
   selector: 'app-grid-cards',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grid-cards.component.scss']
 })
 export class GridCardsComponent implements OnInit {
-  constructor() {}
-  ngOnInit(): void {}
+
+  id:string="0" 
+  cardPhoto:string="" 
+  cardFlag:string="" 
+  cardTitle:string=""
+  cardAuthor:string="" 
+  cardDate:string=""
+  
+  posts:any[] = dataMock;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  
+  }  
 }
