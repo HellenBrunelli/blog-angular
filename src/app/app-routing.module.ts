51f2206dcf7663aac746fb5f92170b509cfbfa9f
Hellen Brunelli <hellenbrunelli01@gmail.com>
2023-09-13 11:34:13 -0300

    [ROUTEs] Routes created.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { PostComponent } from './pages/post/post.component';
+
+const routes: Routes = [
+  { path:'', component: HomeComponent },
+  { path: 'post/:id', component: PostComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
