cdd83e538047377c821d0e421e260270f8cfdf17
Hellen Brunelli <hellenbrunelli01@gmail.com>
2023-09-15 16:34:16 -0300

    [ROUTES] New routes created.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { PostComponent } from './pages/post/post.component';
+import { AllpostsComponent } from './pages/allposts/allposts.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
 const routes: Routes = [
-  { path:'', component: HomeComponent },
-  { path: 'post/:id', component: PostComponent }
+  { path:'', component: HomeComponent, pathMatch: 'full'},
+  { path: 'post/:id', component: PostComponent },
+  { path: 'allposts', component: AllpostsComponent },
+  { path: '**', component: NotfoundComponent }
 ];
 
 @NgModule({
