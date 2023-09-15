08eff2423c4ab30c607f8db9a7d96636b40290e8
Hellen Brunelli <hellenbrunelli01@gmail.com>
2023-09-15 16:49:30 -0300

    [Pages] Adding new pages to the module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 // Components
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { HeroBannerComponent } from './components/hero-banner/hero-banner.component';
+import { GridCardsComponent } from './components/grid-cards/grid-cards.component';
 import { AdsPlaceComponent } from './components/ads-place/ads-place.component';
 import { CardComponent } from './components/card/card.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
-import { GridCardsComponent } from './components/grid-cards/grid-cards.component';
+import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
 
 // Pages
+import { HomeComponent } from './pages/home/home.component';
 import { PostComponent } from './pages/post/post.component';
-import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
+import { AllpostsComponent } from './pages/allposts/allposts.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
 @NgModule({
   declarations: [
@@ -28,10 +31,13 @@ import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.componen
     GridCardsComponent,
     PostComponent,
     BreadcrumbComponent,
+    NotfoundComponent,
+    AllpostsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
