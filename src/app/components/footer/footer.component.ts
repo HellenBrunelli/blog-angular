8f4ea0ead3ae05793ee1ad3ee460cac369ce86ff
Hellen Brunelli <hellenbrunelli01@gmail.com>
2023-09-15 16:43:25 -0300

    [FOOTER] Removed unnecessary functions.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,29 +1,10 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-  isMobile:boolean = true;
-
-  constructor() {
-    this.checkScreenSize();
-  }
-  
-  @HostListener('window:resize', ['$event'])
-  onResize(event: Event): void {
-    this.checkScreenSize();
-  }
-
-  private checkScreenSize():void {
-    const screenWidth = window.innerWidth;
-    const mobileWidth = 768;
-
-    if (screenWidth < mobileWidth) {
-      this.isMobile = true;
-    } else {
-      this.isMobile = false;
-    }
-  }
+  constructor() {}
 }
